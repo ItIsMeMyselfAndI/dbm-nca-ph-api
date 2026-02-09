f38c4d13190e1e4e0a565b25ba4fa12d09aa466f
itismemyselfandi <egermirasol2023@gmail.com>
2026-02-09 16:08:29 +0800

    feat: add requirements.txt - merged use cases similar classes

--- src/core/use_cases/allocation/list_allocations.py
+++ src/core/use_cases/allocation/list_allocations.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from src.core.interfaces.allocation_repository import AllocationRepository
 
 
@@ -5,5 +6,23 @@ class ListAllocations:
     def __init__(self, allocation_repository: AllocationRepository):
         self.allocation_repository = allocation_repository
 
-    def execute(self, cursor: int, limit: int):
-        return self.allocation_repository.list_allocations(cursor, limit)
+    def execute(self, cursor: int, limit: int, filter: Dict[str, str] | None = None):
+        if not filter:
+            return self.allocation_repository.list_allocations(cursor, limit)
+
+        key, value = next(iter(filter.items()))
+
+        if key == "agency":
+            return self.allocation_repository.list_allocations_by_agency(
+                value, cursor, limit
+            )
+        elif key == "nca_number":
+            return self.allocation_repository.list_allocations_by_nca_number(
+                value, cursor, limit
+            )
+        elif key == "operating_unit":
+            return self.allocation_repository.list_allocations_by_operating_unit(
+                value, cursor, limit
+            )
+        else:
+            raise ValueError(f"Unsupported filter key: {key}")
--- src/core/use_cases/record/list_records.py
+++ src/core/use_cases/record/list_records.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from src.core.interfaces.record_repository import RecordRepository
 
 
@@ -5,5 +6,25 @@ class ListRecords:
     def __init__(self, record_repository: RecordRepository):
         self.record_repository = record_repository
 
-    def execute(self, cursor: int, limit: int):
-        return self.record_repository.list_records(cursor, limit)
+    def execute(self, cursor: int, limit: int, filter: Dict[str, str] | None = None):
+        if not filter:
+            return self.record_repository.list_records(cursor, limit)
+
+        key, value = next(iter(filter.items()))
+
+        if key == "department":
+            return self.record_repository.list_records_by_department(
+                value, cursor, limit
+            )
+        elif key == "nca_type":
+            return self.record_repository.list_records_by_nca_type(value, cursor, limit)
+        elif key == "release_id":
+            return self.record_repository.list_records_by_release_id(
+                value, cursor, limit
+            )
+        elif key == "released_date":
+            return self.record_repository.list_records_by_released_date(
+                value, cursor, limit
+            )
+        else:
+            raise ValueError(f"Unsupported filter key: {key}")
